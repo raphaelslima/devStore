b589fd6f9ad838b47e2d77b91d11fdb3746881a3
Raphael Lima <raphaeldesousalm@gmail.com>
2023-03-17 11:58:30 -0300

    Finalizado

--- js/scripts.js
+++ js/scripts.js
@@ -8,48 +8,47 @@ const videos = [
     title: "Formulário Animado com JS puro e CSS Animation | Mayk Brito",
     duration: "57 min",
     thumb: "https://i.ytimg.com/vi/GykTLqODQuU/hqdefault.jpg",
-    video_id: "GykTLqODQuU"
+    video_id: "GykTLqODQuU",
   },
   {
     title: "Desvendando o CSS Grid na prática | Mayk Brito",
     duration: "36 min",
     thumb: "https://i.ytimg.com/vi/HN1UjzRSdBk/hqdefault.jpg",
-    video_id: "HN1UjzRSdBk"
+    video_id: "HN1UjzRSdBk",
   },
   {
     title: "Array: Higher Order Functions | Mayk Brito",
     duration: "54 min",
     thumb: "https://i.ytimg.com/vi/rAzHvYnQ8DY/hqdefault.jpg",
-    video_id: "rAzHvYnQ8DY"
+    video_id: "rAzHvYnQ8DY",
   },
   {
     title: "O que é API? REST e RESTful? | Mayk Brito",
     duration: "33 min",
     thumb: "https://i.ytimg.com/vi/ghTrp1x_1As/hqdefault.jpg",
-    video_id: "ghTrp1x_1As"
+    video_id: "ghTrp1x_1As",
   },
   {
     title: "Desvendando a variável this no Javascript | Mayk Brito",
     duration: "48 min",
     thumb: "https://i.ytimg.com/vi/GSqR2i-Pq6o/hqdefault.jpg",
-    video_id: "GSqR2i-Pq6o"
+    video_id: "GSqR2i-Pq6o",
   },
   {
     title:
       "Como usar Git e Github na prática: Guia para iniciantes | Mayk Brito",
     duration: "33 min",
     thumb: "https://i.ytimg.com/vi/2alg7MQ6_sI/hqdefault.jpg",
-    video_id: "2alg7MQ6_sI"
-  }
+    video_id: "2alg7MQ6_sI",
+  },
 ];
 
-videos.map(video => {
+videos.map((video) => {
   const cardClone = card.cloneNode(true);
   cardClone.setAttribute("id", video.video_id);
   cardClone.querySelector("img").src = video.thumb;
   cardClone.querySelector(".title").innerHTML = video.title;
-  cardClone.querySelector(".info > p.text--medium").innerHTML =
-    video.duration;
+  cardClone.querySelector(".info > p.text--medium").innerHTML = video.duration;
   sectionCards.appendChild(cardClone);
 });
 
@@ -60,7 +59,7 @@ const modalOverlay = document.querySelector(".modal-overlay");
 const modal = document.querySelector(".modal");
 const cards = [...document.querySelectorAll(".cards .card")];
 
-cards.forEach(card => {
+cards.forEach((card) => {
   card.addEventListener("click", () => {
     modal.querySelector(
       "iframe"
@@ -76,4 +75,18 @@ document.querySelector(".close-modal").addEventListener("click", () => {
   modal.classList.remove("active");
   modal.querySelector("iframe").src = ``;
   document.querySelector("body").style.overflow = "initial";
+});
+
+// Menu Responsive
+
+let show = true;
+
+const menuSection = document.querySelector(".menu-section");
+const menuToggle = document.querySelector(".menu-toggle");
+
+menuSection.addEventListener("click", () => {
+  document.body.style.overflow = show ? "hidden" : "initial";
+
+  menuSection.classList.toggle("on", show);
+  show = !show;
 });
